49710ac90d45ea7e9603c4d13ca9c635cd7838b8
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-10-08 19:44:43 -0300

    organizando arquivos

--- cifras-online/src/App.js
+++ cifras-online/src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="app">
-      <head>
-        <title>Cifras Online</title>
-      </head>
-      <div className="header">
-        <input className="input" type="text"></input>
-      </div>
-    </div>
-
-  );
+    return (
+        <div className="app">
+            <head>
+                <title>Cifras Online</title>
+            </head>
+            <div className="header">
+                <input className="input" type="text"></input>
+            </div>
+        </div>
+    );
 }
 
 export default App;
