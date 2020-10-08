deb4c5be87728d2425eb5018dfe62394ccd60aec
Emmanoel Coutinho <61562882+EmmanoelCoutinho@users.noreply.github.com>
2020-10-08 18:42:35 -0300

    frist commit

---
+++ cifras-online/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <head>
+        <title>Cifras Online</title>
+      </head>
+      <div className="header">
+        <input className="input" type="text"></input>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
